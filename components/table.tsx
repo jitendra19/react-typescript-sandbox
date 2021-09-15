b358667348ba6cdbb4e206c249112705a73f61e6
jitendra19 <jiten.cs2010@gmail.com>
2021-09-16 00:33:45 +0530

    working on react-data-table-component lib

---
+++ components/table.tsx
@@ -0,0 +1,29 @@
+import DataTable from 'react-data-table-component';
+
+const columns = [
+  {
+    name: 'Title',
+    selector: row => row.title
+  },
+  {
+    name: 'Year',
+    selector: row => row.year
+  }
+];
+
+const data = [
+  {
+    id: 1,
+    title: 'Beetlejuice',
+    year: '1988'
+  },
+  {
+    id: 2,
+    title: 'Ghostbusters',
+    year: '1984'
+  }
+];
+interface AppProps {}
+export default (props: AppProps) => {
+  return <DataTable columns={columns} data={data} />;
+};
